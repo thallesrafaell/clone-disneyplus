604dbda63791cc80169eb135c745a0e3f84c92f2
Thalles Rafael <thallesrafaell@outlook.com>
2023-10-05 22:31:09 -0300

    ADD IMAGES E  GULP-IMAGEMIN

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
 
 const compilaSass = () => {
-    gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle:'compressed'}))
-    .pipe(gulp.dest('./dist/src/styles'))
+    .pipe(gulp.dest('./dist/styles'))
+    
 }
 
-exports.default = compilaSass;
+function comprimeimg() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
 
+
+exports.default = compilaSass, com;
+exports.img = comprimeimg
 exports.watch = function() {gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.parallel(compilaSass))}
