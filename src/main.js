a26c7382279fcb5331d534cd10b19d4ae4217359
Thalles Rafael <thallesrafaell@outlook.com>
2023-10-10 22:31:33 -0300

    ADICIONANDO SEÇAO DE FAQ

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('[data-tab-button]');
-
+    const questions = document.querySelectorAll('[data-faq-question]')
     
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -13,7 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreFechafaq)
+    }
 })
+
+function abreFechafaq(elemento){
+    const classe = 'faq__questions__item--is-open'
+    const pai = elemento.target.parentNode
+
+    pai.classList.toggle(classe)
+}
 
 function removeBotoes() {
     const buttons = document.querySelectorAll('[data-tab-button]');
