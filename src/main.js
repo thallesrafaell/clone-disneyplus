c7f562d44af2663e686d3998aaf6fca8b1f30e5d
Thalles Rafael <thallesrafaell@outlook.com>
2023-10-11 23:28:24 -0300

    ADCIONANDO O CABECALHO

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]')
+
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+        if(posicaoAtual < alturaHero){
+            ocultaElementosHeader()
+        }else{
+            exibeElementoHeader()
+        }
+    })
     
+
+    //Secao de atracoes, programacao das abas
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAtiva = botao.target.dataset.tabButton;
@@ -14,10 +28,22 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    //Secao de faq acordions
     for(let i = 0; i < questions.length; i++){
         questions[i].addEventListener('click', abreFechafaq)
     }
 })
+
+
+function exibeElementoHeader() {
+    const header = document.querySelector('.header')
+    header.classList.remove('header--is-hidden')
+}
+
+function ocultaElementosHeader() {
+    const header = document.querySelector('.header')
+    header.classList.add('header--is-hidden')
+}
 
 function abreFechafaq(elemento){
     const classe = 'faq__questions__item--is-open'
